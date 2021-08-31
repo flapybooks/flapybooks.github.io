bd74fabaf1e99699e89ba21d0931ca375b3905a9
Andor Chen <andor.chen.27@gmail.com>
2021-08-31 20:18:15 +0800

    Config build

--- config.rb
+++ config.rb
@@ -1,9 +1,21 @@
 # View all configurations: http://localhost:4200/__middleman/config/
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
+# Site
+config[:site] = {
+  name: '笨鸟码书',
+  url: 'https://flapybooks.com',
+  email: 'hi@flapybooks.com',
+  twitter: 'flapybooks',
+  description: '笨鸟码书引进优秀的外文编程资料，翻译成流畅的中文，助力国内开发者成长。我们遵从精益出版策略，精益求精。我们的作品涵盖电子书和视频课，深受 2500+ 开发者的信赖。',
+  fastspring_url: 'https://andor.onfastspring.com',
+  tencent_cloud_public_host: 'https://flapybooks-public-1257356257.file.myqcloud.com',
+  menu_items: {
+    index: '首页',
+    news: '新闻',
+    contact: '联系',
+    login: '登录'
+  }
+}
 
 # Per-page layout changes
 config[:layout] = 'default'
@@ -22,22 +34,15 @@ config[:sass_assets_paths] << Bootstrap.stylesheets_path
 config[:bind_address] = '127.0.0.1'
 config[:port] = '4200'
 
-# Site
-config[:site] = {
-  name: '笨鸟码书',
-  url: 'https://flapybooks.com',
-  email: 'hi@flapybooks.com',
-  twitter: 'flapybooks',
-  description: '笨鸟码书引进优秀的外文编程资料，翻译成流畅的中文，助力国内开发者成长。我们遵从精益出版策略，精益求精。我们的作品涵盖电子书和视频课，深受 2500+ 开发者的信赖。',
-  fastspring_url: 'https://andor.onfastspring.com',
-  tencent_cloud_public_host: 'https://flapybooks-public-1257356257.file.myqcloud.com',
-  menu_items: {
-    index: '首页',
-    news: '新闻',
-    contact: '联系',
-    login: '登录'
-  }
-}
+# Build
+set :skip_build_clean do |path|
+  path =~ /\.nojekyll|\.git/
+end
+
+configure :build do
+  activate :minify_css
+  activate :gzip
+end
 
 # Products
 activate :blog do |blog|
@@ -50,3 +55,6 @@ end
 
 # Extensions
 activate :directory_indexes
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
